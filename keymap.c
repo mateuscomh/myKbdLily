f2febbc90413e610136a7ea89a3cb889a2a35c1e
Matheus Martins <matheuscumth@gmail.com>
2023-08-01 09:57:53 -0300

    Update keymap.c Add pipe

--- keymap.c
+++ keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |  UP  |      |      |      |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   -  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |LEFT | DOWN | RIGHT |      |      |-------.    ,-------|      |      |      |      |      |      |
- * |------+------+------+------+------+------| ENTER |    |       |------+------+------+------+------+------|
+ * |------+------+------+------+------+------| ENTER |    |   |   |------+------+------+------+------+------|
  * |   `  |   !  |   @  |   #  |   $  |   %  |-------|    |-------|      |   _  |   +  |   {  |   }  |   |  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |LOWER | /Space  /       \Backs \  |RAISE |DELETE| RALT |
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   _______, _______,  KC_UP, _______, _______, _______,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
   _______, KC_LEFT,  KC_DOWN, KC_RIGHT, _______, _______,                   _______, _______, _______,_______, _______, _______,
-  KC_GRV, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_ENT, _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+  KC_GRV, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_ENT, KC_PIPE, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
                              _______, _______, _______, _______, _______,  _______, _______, _______
 ),
 /* RAISE

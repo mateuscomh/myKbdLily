01b5eb611148a707f1d04e9dc2e529bb9c8da0ef
mateuscomh <matheuscumth@gmail.com>
2024-12-10 10:10:31 -0300

    update layout

--- keymap.c
+++ keymap.c
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | M.U  |      |  UP  |      |      | MB.1 |                    |      |      |      |   (  |   )  |   |/ |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | M.D  |LEFT | DOWN | RIGHT |      | MB.2  |-------.    ,-------|      |      |      |      |      |      |
+ * | M.D  |LEFT | DOWN | RIGHT |      | MB.2  |-------.    ,-------|      |      |      |      |      |  `  |
  * |------+------+------+------+------+------| ENTER |    |   |/  |------+------+------+------+------+------|
  * | CAPS | MLef | MDow | MUP  | MRig |   =  |-------|    |-------|      |      |      |      |  \   |   |  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   KC_WH_U, _______,  KC_UP, _______, _______, KC_BTN1,                   _______, _______, _______, KC_LPRN, KC_RPRN, KC_BSLS,
-  KC_WH_D, KC_LEFT,  KC_DOWN, KC_RIGHT, _______, KC_BTN2,                _______, _______, _______,_______, _______, _______,
+  KC_WH_D, KC_LEFT,  KC_DOWN, KC_RIGHT, _______, KC_BTN2,                _______, _______, _______,_______, _______, KC_GRV,
   CAPS_HOLD,  KC_MS_L, KC_MS_D, KC_MS_U,  KC_MS_R, KC_PEQL, KC_ENT,  KC_BSLS, _______, _______, _______, _______, KC_BSLS, KC_PIPE,
                         KC_BSPC, _______, _______, _______,       _______,  _______, _______, _______
 ),
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   \   |    |   /   |------+------+------+------+------+------|
  * |  F7  |  F8  |  F9  | F10  | F11  | F12  |-------|    |-------|   +  |   -  |   *  |   /  |   =  |  END |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Backs \  |RAISE | RCTL | RGUI |
+ *                   | LAlt | LGUI |LOWER | /Space  /       \Backs \  |RAISE | RCTL | PSCR |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -109,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PGUP,
   KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,                       KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,   XXXXXXX, KC_PGDN,
   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSLS, KC_SLSH,  KC_PLUS, KC_MINS, KC_PAST,  KC_PSLS, KC_PEQL, KC_END,
-                             _______, _______, _______,  _______, _______,  _______, KC_RCTL, KC_ENT
+                             _______, _______, _______,  _______, _______,  _______, KC_RCTL, KC_PSCR
 ),
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.

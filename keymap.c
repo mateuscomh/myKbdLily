4310847b082df41d01cf6d26734aaec103172f88
mateuscomh <matheuscumth@gmail.com>
2025-08-21 10:58:29 -0300

    chore: arrow keys on right side

--- keymap.c
+++ keymap.c
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LOWER] = LAYOUT(
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
         MS_WHLU, _______, KC_UP,   _______, MS_WHLL, MS_BTN1,                      _______, _______, _______, KC_LPRN, KC_RPRN, KC_BSLS,
-        MS_WHLD, KC_LEFT, KC_DOWN, KC_RGHT, MS_WHLR, MS_BTN2,                      _______, _______, _______, _______, _______, KC_GRV,
+        MS_WHLD, KC_LEFT, KC_DOWN, KC_RGHT, MS_WHLR, MS_BTN2,                      KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, KC_GRV,
         _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, MS_BTN3, KC_ENT,    KC_BSLS,  _______, _______, _______, _______, KC_BSLS, KC_PIPE,
                                  KC_BSPC, _______, _______, _______,   _______,  _______, _______, _______
     ),

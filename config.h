12c22fe259e2fa8f2b2ce2ca5417540548ca1f9e
mateuscomh <matheuscumth@gmail.com>
2025-09-14 13:04:16 -0300

    fix: MOUSE keys update with oficial doc

--- config.h
+++ config.h
@@ -28,16 +28,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Timeout to check side
 #define SPLIT_USB_TIMEOUT 1000
 #define SPLIT_WATCHDOG_ENABLE
-#define SPLIT_WATCHDOG_TIMEOUT 3000
+#define SPLIT_WATCHDOG_TIMEOUT 1500
 
 // oled configs
-#define OLED_TIMEOUT 300000
-#define OLED_BRIGHTNESS 130
+#define OLED_TIMEOUT 360000
+#define OLED_BRIGHTNESS 150
 
-#define MOUSE_DELAY 12
-#define MOUSEKEY_MAX_SPEED 2
-#define MOUSE_TIME_TO_MAX 90
-#define MOUSE_MOVE_DELTA 4
+#define MOUSEKEY_DELAY 12
+#define MOUSEKEY_MAX_SPEED 4
+#define MOUSEKEY_TIME_TO_MAX 50
+#define MOUSEKEY_MOVE_DELTA 5
 #define MOUSEKEY_INTERVAL 16
 
 
@@ -46,7 +46,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Previne que modificadores fiquem "presos" em algumas situações */
 #define PREVENT_STUCK_MODIFIERS
 
-#define USB_POOLING_INTERVAL_MS 10
+#define USB_POLLING_INTERVAL_MS 10
 #define SPLIT_WPM_ENABLE
 #define TAPPING_TERM 175
 #define TAPPING_TERM_PER_KEY
